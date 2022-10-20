bad6bbfd619ff8f2edf3011e72e404f59c768156
Zachary Fried <zatfried@gmail.com>
2022-10-20 15:33:59 -0400

    Added functionality to multiple time zones to better address participants without time zone data

--- README_RAPIDS_HELPER.md
+++ README_RAPIDS_HELPER.md
@@ -10,21 +10,23 @@ python create_rapids_participant_file.py --mysqlconfig <.my.cnf location> --data
 ```
 
 - This file was created because Rapids removed the automatic pulling of participant files from the “aware_device” table.  It will pull data from an aware_device formatted table and turn it into an aware_csv file that rapids can read.
-- By default it will use your `~/.my.cnf` file.  The other 3 options should be filled in by the database and table you want to pull your AWARE participants from, and the destination file should be where you want the resulting .csv file to be located (you will probably want to use “../../” + the string listed in the “CSV_FILE_PATH” in your `config.yaml` file.
+- By default it will use your `~/.my.cnf` file.  The other 3 options should be filled in by the database and table you want to pull your AWARE participants from, and the destination file should be where you want the resulting .csv file to be located (you will probably want to use “../../” + the string listed in the “CSV_FILE_PATH” in your `config.yaml` file. 
 
-### Creating RAPIDS TZCODES_FILE (multiple time zones csv)
+### Creating RAPIDS TZCODES_FILE (multiple time zones CSV file)
 
 `src/data/create_multiple_timezones.py:`
 
 ```python
 python create_multiple_timezones.py  --database <database name> --device_source_table <tablename> 
 					--survey_source_table <tablename> --survey_col_name <name of label column in survey table>
-					 [--mysqlconfig <.my.cnf location>] [--destination_file <full path of output file>]
-			  [--default_tz <time zone setting>], [--participant_directory <full path of participant_files directory>]
+					[--mysqlconfig <.my.cnf location>] [--destination_file <full path of output file>]
+                    [--tz_default <set default for data missing time zones>] 
+                    [--participant_input <path to participant file CSV>] 
+                    [--participant_output <desired path for modified participant CSV>]
 ```
 
 - This script creates a TZCODES_FILE (a CSV file containing the time zones in which participants’ devices sensed data) that can be supplied to RAPIDS in the `config.yaml` under `[TIMEZONE][MULTIPLE][TZCODES_FILE]`
-- This script requires a survey_source_table that has two columns: a column that will be used to join this table to the aware_device table using the label column, and a column named “time_zone” with integers corresponding to different time zones as dictated by the table below.
+- This script requires a survey_source_table that has two columns: a column that will be used to join this table to the aware_device table using the label column, and a column named “time_zone” with integers corresponding to different time zones as outlined by the table below.
 
 | Integer | Time zone | tzcode to be applied |
 | --- | --- | --- |
@@ -36,7 +38,8 @@ python create_multiple_timezones.py  --database <database name> --device_source_
 | 6 | Alaskan Standard Time (AKST) | America/Anchorage |
 | 7 | Hawaii–Aleutian Standard Time (HST) | Pacific/Honolulu |
 
-- This script requires specifying the database in which your data is located, the name of the aware_device table, the name of the time zone survey table, and the name of the column in your time zone survey table that matches the “label” column of the aware_device table. Default location of your mysqlconfig is `~/.my.cnf`. Default output destination is `../../../data/external/multiple_timezones.csv`.
+- This script requires specifying the database in which your data is located, the name of the aware_device table, the name of the time zone survey table, and the name of the column in your time zone survey table that matches the “label” column of the aware_device table. 
+- Default location of your mysqlconfig is `~/.my.cnf`. Default output destination is `../../../data/external/multiple_timezones.csv`. Default path to participant CSV file created previously by `create_participant_file.py` is `../../../data/external/participant_file.csv`.
 - An example survey_source_table with label column “eid” is below:
 
 | eid | time_zone |
@@ -45,4 +48,14 @@ python create_multiple_timezones.py  --database <database name> --device_source_
 | AA10307 | 2 |
 | AA12108 | 2 |
 
-- A default tzcode can be specified with the `default_tz` option (i.e. `--default_tz America/New_York`) which is then used for participants with missing time zone data. Using argument "ignore" results in the deletion of the `.yaml` files for participants with missing time zone data so that RAPIDS ignores them. The default for `default_tz` simply raises an exception if there are participants with missing time zone data so that this can be addressed before using RAPIDS. When using argument "ignore", please specify the path to the participant_files directory with `--participant_directory` if different than default of `../../../data/external/participant_files/`.
+- Use option `tz_default` to decide how participants whose labels are not found in the time zone survey table are addressed. Default option is `remove`. See table for the possible arguments and their behaviors 
+
+| Argument | Behavior |
+| --- | --- |
+| remove | Creates TZCODES file with only participants that have time zone data. Also creates new participant file so that participants without time zone data are not included by RAPIDS. |
+| ignore | Creates TZCODES file with only participants that have time zone data. Notifies user if there are participants specified in the partipant file without time zone data and instructs user to edit `config.yaml` to set a default time zone for participants with mising data. |
+| custom tzcode | User passes a tzcode as the argument (e.g. 'America/New_York') which is applied to participants without time zone data, permitting the creation of a TZCODES file including all participants in the device source table. |
+
+- If using argument `remove` for option `tz_default`, the default output destination for the modified participant file is `../../../participant_file_modified.csv`. A different destination can be specified with option `--participant_output`.
+- If using argument `ignore`, user will need to change values of `[IF_MISSING_TZCODE]` and/or `[DEFAULT_TZCODE]` under `[TIMEZONE][MULTIPLE]` in `config.yaml`. Refer to https://www.rapids.science/1.9/setup/configuration/#timezone-of-your-study for reference.
+
--- src/data/create_multiple_timezones.py
+++ src/data/create_multiple_timezones.py
@@ -23,30 +23,31 @@ import numpy as np
 from pathlib import Path
 import getopt
 import sys
-import os 
 
 def usage():
     print("Error: Unknow options.  Please run with the correct arguments:")
-    print("python create_multiple_timezones.py  --database <database name> --device_source_table <tablename> --survey_source_table <tablename> --survey_col_name <name of column in timezones_survey that matches label column of aware_device table> ")
-    print("                                         [--mysqlconfig <.my.cnf location>] [--destination_file <full path of output file>] [--default_tz <time zone setting>], [--participant_directory <full path of participant_files directory>]")
+    print("python create_multiple_timezones.py  --database <database name> --device_source_table <tablename> --survey_source_table <tablename> --survey_col_name <name of col in survey source table that matches 'label' col of device source table> ")
+    print("                                         [--mysqlconfig <.my.cnf location>] [--destination_file <full path of output file>] ")
+    print("                                         [--tz_default <default for missing time zone data>] [--participant_input <path to participant file CSV>] [--participant_output <desired path for modified participant CSV>]")
 
 
 def main():
 
     try:
-        optlist, args = getopt.getopt(sys.argv[1:], "", ["mysqlconfig=", "database=", "device_source_table=", "survey_source_table=", "survey_col_name=", "destination_file=", "default_tz=", "participant_directory="])
+        optlist, args = getopt.getopt(sys.argv[1:], "", ["mysqlconfig=", "database=", "device_source_table=", "survey_source_table=", "survey_col_name=", "destination_file=", "tz_default=", "participant_input=", "participant_output="])
     except getopt.GetoptError as err:
         print(err)
         usage()
         sys.exit(2)
 
-    #Set default options   
+    # Set default options
+    print("Creating multiple_timezones.csv...")    
     options = {}
     options["mysqlconfig"] = str(str(Path.home()) + '/.my.cnf')
     options["destination_file"] = "../../../data/external/multiple_timezones.csv"
-    options["default_tz"] = "no_default"
-    options["participant_directory"] = "../../../data/external/participant_files/"
-    custom_tz_message = ""
+    options["tz_default"] = "remove"
+    options["participant_input"] = "../../../data/external/participant_file.csv"
+    options["participant_output"] = "../../../data/external/participant_file_modified.csv"
     
     # OVERRIDE GENERAL DEFAULTS WITH COMMAND LINE ARGUMENTS
     for option_tuple in optlist:    
@@ -62,10 +63,12 @@ def main():
             options["survey_col_name"] = option_tuple[1]
         elif (option_tuple[0] == "--destination_file"):
             options["destination_file"] = option_tuple[1]
-        elif (option_tuple[0] == "--default_tz"):
-            options["default_tz"] = option_tuple[1]
-        elif (option_tuple[0] == "--participant_directory"):
-            options["participant_directory"] = option_tuple[1]
+        elif (option_tuple[0] == "--tz_default"):
+            options["tz_default"] = option_tuple[1]
+        elif (option_tuple[0] == "--participant_input"):
+            options["participant_input"] = option_tuple[1]
+        elif (option_tuple[0] == "--participant_output"):
+            options["participant_output"] = option_tuple[1]
 
     if (not "database" in options or not "device_source_table" in options or not "survey_source_table" in options or not "survey_col_name" in options):
         usage()
@@ -74,16 +77,35 @@ def main():
     # Force Connection type by using Loopback IP address.
     myDB = db.engine.URL.create(drivername="mysql", host="127.0.0.1", database = options["database"], query={"read_default_file" : options["mysqlconfig"]})
 
-    #engine = db.create_engine(name_or_url=myDB, pool_pre_ping=True)            
+    # engine = db.create_engine(name_or_url=myDB, pool_pre_ping=True)            
     engine = db.create_engine(myDB, pool_pre_ping=True)
 
-    # create PANDAS dataframes from SQL tables, converting time_zone column to stings for next step
-    device_df = pd.read_sql_table(options["device_source_table"], engine)
-    survey_df = pd.read_sql_table(options["survey_source_table"], engine)
-    joined_df = pd.merge(device_df, survey_df, left_on='label', right_on=options["survey_col_name"])
+    # create PANDAS dataframes from SQL tables
+    survey_df = pd.read_sql_table(options["survey_source_table"], engine) # particiapt label, time_zone integer
+    participant_df = pd.read_csv(options["participant_input"]) #  participant label, device_id
+    device_df = pd.read_sql_table(options["device_source_table"], engine, columns=['device_id', 'timestamp']) # device_id, timestamp
+
+    # Create table with device_id, participant label, and time_zone integer
+    joined_nostamp_df = pd.merge(participant_df, survey_df, left_on="label", right_on=options["survey_col_name"], how='left')
+
+    # Address the fact that some rows in joined_nostamp_df have multiple device_ids per row by creating a dataframe with one row per device_id which is then appended to joined_nostamp_df. Duplicates deleted later. 
+    to_add_df = pd.DataFrame(columns=list(joined_nostamp_df.columns))
+    for index, row in joined_nostamp_df.iterrows():
+        id_list = row["device_id"].split(';')
+        if len(id_list) > 1:
+            for id in id_list:
+                to_add_df = to_add_df.append({'device_id' : id, 'pid' : row['pid'], 'label' : row['label'], 'platform' : row['platform'], 'start_date' : row['start_date'], 'end_date' : row['end_date'], 'eid' : row['eid'], 'time_zone' : row['time_zone'], 'time_zone_text' : row['time_zone_text']}, ignore_index = True)
+        else:
+            pass
+    joined_nostamp_df = joined_nostamp_df.append(to_add_df)
+    
+    # Adds timestamp data from device_df 
+    joined_df = pd.merge(joined_nostamp_df, device_df, how='left', on='device_id') 
+
+    # time_zone integers converted to strings
     joined_df['time_zone'] = joined_df['time_zone'].astype(str)
 
-    #time_zone column which represents timezones as integers formated as strings is used to create column tzcode
+    # Column tzcode is populated with tz codes based on value of time_zone
     joined_df['tzcode'] = np.where(joined_df['time_zone'].str.startswith('1'), 'America/Puerto_Rico', 'TBD')
     joined_df['tzcode'] = np.where(joined_df['time_zone'].str.startswith('2'), 'America/New_York', joined_df['tzcode'])
     joined_df['tzcode'] = np.where(joined_df['time_zone'].str.startswith('3'), 'America/Chicago', joined_df['tzcode'])
@@ -91,35 +113,74 @@ def main():
     joined_df['tzcode'] = np.where(joined_df['time_zone'].str.startswith('5'), 'America/Los_Angeles', joined_df['tzcode'])
     joined_df['tzcode'] = np.where(joined_df['time_zone'].str.startswith('6'), 'America/Anchorage', joined_df['tzcode'])
     joined_df['tzcode'] = np.where(joined_df['time_zone'].str.startswith('7'), 'Pacific/Honolulu', joined_df['tzcode'])
-    if options["default_tz"] == "no_default": #User wants to pass all participants to RAPIDS and assumes there are no missing time zone data. Exception raised if there are missing data, passes if there are none. 
-        if joined_df["tzcode"].str.contains('TBD').sum() > 0:
-            print("ERROR: There was at least one missing value in the time_zone column. You have not set a default time zone to replace null values. Please set a default TZ database name to replace null values or use argument 'ignore' to skip participants with no time_zone data. A list of TZ database names can be found at https://en.wikipedia.org/wiki/List_of_tz_database_time_zones")
-            exit(2)
-        else:
-            pass
-    elif options["default_tz"] == "ignore": #User wants to use RAPIDS while ignoring participants for which there is missing time zone data. Deletes .yaml files in participant_files folder for these participants. 
-        to_delete = [] 
-        for index, row in joined_df.iterrows():
-            if row['tzcode'] == 'TBD':
-                to_delete.append(row['label'])
-        for i in to_delete:
-            file_name = options["participant_directory"] + i + ".yaml"
-            if os.path.exists(file_name):
-                os.remove(file_name)
-            else:
-                pass
-        print("Ignored " + str(len(to_delete)) + " participants without time zone data. Their participant files have been removed. Please re-run create_participant_files.py if you wish to include these participants in the future.")
-    else: #User is specififying a custom TZ database name to use for participants with missing time zone data.
-        custom_tz_message = "You have set custom TZ database name of: " + options["default_tz"] + ". Please ensure this is a valid time zone in the TZ database before proceeding to use of RAPIDS. A list of TZ database names can be found at https://en.wikipedia.org/wiki/List_of_tz_database_time_zones"
-        joined_df['tzcode'] = np.where(joined_df['tzcode'].str.startswith('TBD'), options["default_tz"], joined_df['tzcode'])
 
-    #A dataframe is created with the columns of interest for RAPIDS and is exported as a csv to '/rapids/data/external/multiple_timezones.csv'
+    # tz_default option applied in following if loop:
+    # Remove participants from participant CSV file that do not have time zone data. Create multiple_timezone.csv including only remaining participants. 
+    if options["tz_default"] == "remove": 
+
+        # Create df that acts as a reference of device_ids that have a time zone specified in the survey data
+        ids_with_tz = pd.merge(participant_df, survey_df, left_on="label", right_on=options["survey_col_name"])
+
+        # Remove rows from particiant_df where there is a device_id without a time zone code
+        participant_df['drop_bool'] = 0
+        for index, row in participant_df.iterrows():
+            id_list = row["device_id"].split(';')
+            for id in id_list:
+                if ids_with_tz['device_id'].str.contains(id).any() == False: # now I need to make it so that it checks a dataframe or list of ids that isn't all of participant ids 
+                    participant_df.loc[index, 'drop_bool'] += 1
+                else:
+                    pass
+        participant_df = participant_df[participant_df['drop_bool'] == 0]
+        participant_df = participant_df.drop('drop_bool', axis = 1)
+
+        # Create new participant file with only participants that have time zones specified in the survey table
+        participant_df.to_csv(options["participant_output"], index=False)
+
+        # Remove rows from joined_df where there is a device_id without a time zone code
+        joined_df['drop_bool'] = 0
+        for index, row in joined_df.iterrows():
+            id_list = row["device_id"].split(';')
+            for id in id_list:
+                if ids_with_tz['device_id'].str.contains(id).any() == False: # now I need to make it so that it checks a dataframe or list of ids that isn't all of joined ids 
+                    joined_df.loc[index, 'drop_bool'] += 1
+                else:
+                    pass
+        joined_df = joined_df[joined_df['drop_bool'] == 0]
+        joined_df = joined_df.drop('drop_bool', axis = 1).drop_duplicates(['device_id'], keep='first')
+        
+        message = "A new participant CSV file has been saved as " + options["destination_file"] + ". Indicate this location in config.yaml under [CREATE_PARTICIPANT_FILES][CSV_FILE_PATH] before creating participant files with RAPIDS."
+
+    # Simply print how many participants there are in the participant file with missing time zone data. 
+    elif options["tz_default"] == "ignore": 
+
+        #Checks to see if there are any participants with missing tzcodes. 
+        if (joined_df["tzcode"].eq('TBD')).any() == True:
+            message = "There were " + str(joined_df["tzcode"].eq('TBD').sum()) + " participants with missing tz codes. You will need to change values of [IF_MISSING_TZCODE] and/or [DEFAULT_TZCODE] under [TIMEZONE][MULTIPLE] in config.yaml. Refer to https://www.rapids.science/1.9/setup/configuration/#timezone-of-your-study for reference."
+            joined_df = joined_df[joined_df.tzcode != 'TBD']
+        else:
+            message = "There were no participants with missing tz codes."
+        
+        #Drop duplicate rows which occurs for participants that responded to the study survey multiple times
+        joined_df = joined_df.drop_duplicates(['device_id'], keep='first')
+
+    # Fills missing tzcodes with custom value from user
+    else:
+        joined_df['tzcode'] = np.where(joined_df['tzcode'].str.startswith('TBD'), options['tz_default'], joined_df['tzcode'])
+        message = "You selected to use a time zone of '" + options['tz_default'] + "' for any participants that do not have time zone data."
+
+        # Drop duplicate rows which occurs for participants that responded to the study survey multiple times
+        joined_df = joined_df.drop_duplicates(['device_id'], keep='first')
+    
+    # Removes rows with multiple device_ids per row which were already used to create new rows with one device_id per row earlier
+    joined_df = joined_df.dropna(subset=['timestamp'])
+
+    # A dataframe is created with the columns of interest for RAPIDS and is exported as a csv with path specified by options["destination_file"]
     final_df = joined_df[['device_id', 'tzcode', 'timestamp']]
     final_df.to_csv(options["destination_file"], index=False)
-    if len(custom_tz_message) > 0:
-        print(custom_tz_message)
-    print("Successfully created /rapids/data/external/multiple_timezones.csv. Please change file path field in [TIMEZONE][TZCODES_FILE] config.yaml as needed.")
+    print(message)
+    print("Created " + options["destination_file"] + ". Please change file path field in [TIMEZONE][TZCODES_FILE] config.yaml as needed.")
 
 if __name__ == "__main__":
 
     main()
+    
